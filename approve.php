e3482843525183bd28994e5a072ff03105a94837
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-06-22 11:26:39 -0700

    update approve file

--- approve.php
+++ approve.php
@@ -9,8 +9,11 @@ global $wpdb;
 /**
  * solution is to update the sql query so that it only
  * queries ONE record, the most recent one.
- * 
+ * TODO
+ * find this sql code on some document and find out how it does the update process
+ * then request that it only updates one not all.
  */
+
 
 $wpdb->update($wpdb->prefix . 'adoptedideaswidget',
 	array(
@@ -24,3 +27,5 @@ $wpdb->update($wpdb->prefix . 'adoptedideaswidget',
 messages_delete_thread($bp_id);
 
 header("Location: ".$_SERVER['HTTP_REFERER']);
+
+
